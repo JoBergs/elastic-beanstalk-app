baff191534c76b39945141da3e96e22a8ed547a9
JoBergs <johannes.bergs@datamics.com>
2023-07-19 15:17:01 -0300

    Apply all code changes

--- eb_app.tf
+++ eb_app.tf
@@ -7,21 +7,21 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "enes-mybucket"  # S3 bucket name
-    key    = "path/to/my/key" # S3 key name
-    region = "us-east-2"      # S3 region
+    bucket = "eb-flask-app-statefile"  # S3 bucket name
+    key    = "statefile/terraform.tfstate" # S3 key name
+    region = "eu-central-1"      # S3 region
   }
 }
 
 # AWS Provider configuration
 provider "aws" {
   profile = "default"   # AWS profile 
-  region  = "us-east-2" # AWS region
+  region  = "eu-central-1" # AWS region
 }
 
 # Create S3 bucket for Python Flask app
 resource "aws_s3_bucket" "eb_bucket" {
-  bucket = "enes-eb-python-flask" # Name of S3 bucket to create for Flask app deployment needs to be unique 
+  bucket = "jo-eb-python-flask" # Name of S3 bucket to create for Flask app deployment needs to be unique 
 }
 
 # Define App files to be uploaded to S3
@@ -42,7 +42,7 @@ resource "aws_elastic_beanstalk_application_version" "eb_app_ver" {
   bucket      = aws_s3_bucket.eb_bucket.id                    # S3 bucket name
   key         = aws_s3_bucket_object.eb_bucket_obj.id         # S3 key path 
   application = aws_elastic_beanstalk_application.eb_app.name # Elastic Beanstalk application name
-  name        = "enes-eb-tf-app-version-lable"                # Version label for Elastic Beanstalk application
+  name        = "jo-eb-tf-app-version-label"                # Version label for Elastic Beanstalk application
 }
 
 resource "aws_elastic_beanstalk_environment" "tfenv" {
